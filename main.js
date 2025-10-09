af6f62a41c57dc2720710f5e9f512d08e2e7088c
Ryan <65054422+Ryanjones0911@users.noreply.github.com>
2025-10-08 23:08:50 -0500

    playing with naive lighting, just ThreeJS no shaders

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function main() {
     //simple camera
 
     const cameraAttributes = {
-        fov: 75,
+        fov: 80,
         aspectRatio: window.innerWidth / window.innerHeight,
         near: 0.1,
         far: 1000
@@ -14,35 +14,43 @@ function main() {
 
     const camera = new THREE.PerspectiveCamera(cameraAttributes.fov, cameraAttributes.aspectRatio, cameraAttributes.near, cameraAttributes.far);
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x202030);
+
+    const bg = new THREE.TextureLoader().load('assets/8k_stars_milky_way.jpg');
+    scene.background = bg;
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     //define materials for it
-    function newSphere(radius, color, x, y, z) {
+    function newSphere(radius, color, texture = null, x, y, z, options = {}) {
     const geometry = new THREE.SphereGeometry(radius);
-    const material = new THREE.MeshPhongMaterial({color});
+    const material = new THREE.MeshPhongMaterial({
+        map: texture,
+    });
     const sphere = new THREE.Mesh(geometry, material);
     sphere.position.set(x,y,z)
     scene.add(sphere);
     return sphere;
     }
-
-    const sphere = newSphere(2, 0x00ff00, 0, 0, 0);
-    const sphere2 = newSphere(.4, 0xffffff, 6, 0, 0);
+    
+    const sunTexture = new THREE.TextureLoader().load('assets/8k_sun.jpg');
+    const earthTexture = new THREE.TextureLoader().load('assets/8k_earth_daymap.jpg');
+    const sun = newSphere(2, 0x00ff00, sunTexture, 0, 0, 0, {emissive: 0xffaa33, emissiveIntensity: 1000000000});
+    const earth = newSphere(.4, 0xffffff, earthTexture, 6, 0, 0);
     camera.position.z = 5;
 
     //throw in a temp light
-    const light = new THREE.DirectionalLight(0xffffff,2);
-    light.position.set(5,5,5);
+    const light = new THREE.PointLight(0xffffff,100,100);
+    light.position.set(0,0,0);
     scene.add(light);
-    scene.add(new THREE.AmbientLight(0xffffff, .2));
+    scene.add(new THREE.AmbientLight(0xffffff, .4));
 
 
     function animate() {
-        sphere.rotation.x += 2;
+        sun.rotation.y += .002;
+        earth.rotation.y += .002;
+
 
         renderer.render(scene, camera);
     }
